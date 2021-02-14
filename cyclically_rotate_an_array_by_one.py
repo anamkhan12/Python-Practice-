4ce774af1b324f17635b97a3ef20119d50e862d3
Anam Khan <anam612k@gmail.com>
2021-02-14 19:20:15 +0530

    init : python code practice

---
+++ cyclically_rotate_an_array_by_one.py
@@ -0,0 +1,18 @@
+
+def cyclicallyrotate(array,n):
+  temp_last= array[n-1]
+  for i in range(n-1,0,-1):
+    array[i]=array[i-1]
+  array[0] = temp_last
+
+def printRotatedArray(array,n):
+    for i in range(n):
+        print("% d" % array[i],end=" ")
+    print()
+t=int(input())
+for i in range(t):
+    n = int(input())
+    array = list(map(int, input().rstrip().split()))
+    cyclicallyrotate(array,n)
+    printRotatedArray(array,n)
+    
